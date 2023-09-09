ae7ccab64970ae38bc0945915c6a2fa8fdcf8bf4
VuHuyHieu <mino010901@gmail.com>
2023-09-09 11:15:00 +0700

    Lan1

---
+++ src/main/java/vn/com/edu/fit/week01/models/GrantAccess.java
@@ -0,0 +1,60 @@
+package vn.com.edu.fit.week01.models;
+
+public class GrantAccess {
+    private String accountId;
+    private String roleId;
+    private String isGrant;
+    private String note;
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getIsGrant() {
+        return isGrant;
+    }
+
+    public void setIsGrant(String isGrant) {
+        this.isGrant = isGrant;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public GrantAccess() {
+    }
+
+    public GrantAccess(String accountId, String roleId, String isGrant, String note) {
+        this.accountId = accountId;
+        this.roleId = roleId;
+        this.isGrant = isGrant;
+        this.note = note;
+    }
+
+    @Override
+    public String toString() {
+        return "GrantAccess{" +
+                "accountId='" + accountId + '\'' +
+                ", roleId='" + roleId + '\'' +
+                ", isGrant='" + isGrant + '\'' +
+                ", note='" + note + '\'' +
+                '}';
+    }
+}
